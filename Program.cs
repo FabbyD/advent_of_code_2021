26302f0c7059ce5ca23bbde4a61919ecd6bbdbe9
Fabrice Dugas <fabby.dugas@gmail.com>
2021-12-05 17:20:30 -0500

    Initial project structure and solution for puzzle 1

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using AdventOfCode2021.Puzzles;
+
+namespace AdventOfCode2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                var challenge = new Puzzle1(args[0]);
+                challenge.Solve();
+            }
+            else
+            {
+                Console.WriteLine("No arguments found!");
+            }
+        }
+    }
+}
---
+++ puzzles/Puzzle1.cs
@@ -0,0 +1,49 @@
+using System;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2021.Puzzles
+{
+    public class Puzzle1
+    {
+        private readonly string inputPath;
+
+        public Puzzle1(string inputPath)
+        {
+            this.inputPath = inputPath;
+        }
+
+        public void Solve()
+        {
+            try
+            {
+                int[] measurements = File.ReadAllLines(inputPath)
+                    .Select(line => int.Parse(line)) // could throw exception...
+                    .ToArray();
+                Solve(measurements);
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("Could not find input file at : {0}", inputPath);
+            }
+        }
+
+        private void Solve(int[] measurements)
+        {
+            int previous = measurements[0];
+            var increasedCount = measurements
+                .Skip(1)
+                .Aggregate(0, (count, next) => 
+                {
+                    if (next > previous)
+                    {
+                        count++;
+                    }
+                    previous = next;
+                    return count;
+                });
+
+            Console.WriteLine("Solution: {0}", increasedCount);
+        }
+    }
+}
