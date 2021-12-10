38f8f50df325d49aa515d5123eed137bc489c1f2
Fabrice Dugas <fabby.dugas@gmail.com>
2021-12-09 22:13:37 -0500

    Day 6

---
+++ days/Day6.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2021.Days
+{
+    public class Day6 : IDay
+    {
+        class School
+        {
+            private List<long> fishes;
+
+            private const int TimeToReproduce = 7;
+            private const int Delay = 2;
+
+            public long Count => fishes.Sum();
+
+            public School()
+            {
+                fishes = new List<long>(TimeToReproduce+Delay);
+                for (int i = 0; i < TimeToReproduce+Delay; i++)
+                {
+                    fishes.Add(0);
+                }
+            }
+
+            public void AddFish(int timer)
+            {
+                fishes[timer]++;
+            }
+
+            public void Day()
+            {
+                // keep track of new fishes but only add them at the end of the day
+                var newFishCount = fishes[0];
+
+                for (int i = 0; i < TimeToReproduce + Delay - 1; i++)
+                {
+                    fishes[i] = fishes[i+1];
+                }
+                
+                fishes[TimeToReproduce+Delay-1] = newFishCount;
+                fishes[TimeToReproduce-1] += newFishCount;
+            }
+        }
+
+        private readonly string inputPath = "inputs/day6.txt";
+
+        public void Part1()
+        {
+            Simulate(80);
+        }
+
+        public void Part2()
+        {
+            Simulate(256);
+        }
+
+        private void Simulate(int dayCount)
+        {
+            var school = new School();
+            var timers = File.ReadAllLines(inputPath)[0].Split(",");
+            foreach (var timer in timers)
+            {
+                school.AddFish(int.Parse(timer));
+            }
+
+            for (int i = 0; i < dayCount; i++)
+            {
+                school.Day();
+            }
+
+            Console.WriteLine("Solution : {0}", school.Count);
+        }
+    }
+}
