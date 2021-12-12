7b4ae654c138303020154f6f218aa9ca962f41f0
Fabrice Dugas <fabby.dugas@gmail.com>
2021-12-12 17:26:44 -0500

    Moved everything under csharp/

--- csharp/days/Day.cs
+++ csharp/days/Day.cs
@@ -4,7 +4,7 @@ namespace AdventOfCode2021.Days
     {
         public bool UseExample { get; set;}
 
-        protected virtual string inputPath => $"inputs/{GetType().Name.ToLower()}{(UseExample ? "_example" : string.Empty)}.txt";
+        protected virtual string inputPath => $"../inputs/{GetType().Name.ToLower()}{(UseExample ? "_example" : string.Empty)}.txt";
 
         public abstract ulong Part1();
         public abstract ulong Part2();
